844d07d3db6f49ce4742f9ceb2851c0b54fef838
anasa <anas-aji-seeroo>
2023-12-04 22:43:44 +0000

    first commit

---
+++ backend/src/test/java/com/anasajimuhammed/financebackend/FinancebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anasajimuhammed.financebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
